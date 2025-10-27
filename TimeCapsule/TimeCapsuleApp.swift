d73890d1b81b47663ce01ff1081a7e42838f419f
Samuel Martins <fsamuelsmartins@gmail.com>
2025-10-27 09:25:16 -0300

    Initial Commit

---
+++ TimeCapsule/TimeCapsuleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeCapsuleApp.swift
+//  TimeCapsule
+//
+//  Created by Samuel Martins on 27/10/25.
+//
+
+import SwiftUI
+
+@main
+struct TimeCapsuleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
