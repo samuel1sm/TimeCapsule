0f574ab018e7bddc58d150ef37b234d7d4c93f44
Samuel Martins <fsamuelsmartins@gmail.com>
2025-11-03 15:54:19 -0300

    creating view is loading function

--- TimeCapsule/Capsule/CreateCapsule/Components/PhotosAndVideosView.swift
+++ TimeCapsule/Capsule/CreateCapsule/Components/PhotosAndVideosView.swift
@@ -5,16 +5,9 @@ struct PhotosAndVideosView: View {
 	@Binding var selectedItems: [PhotosPickerItem]
 	@State private var selectedImages: [Image] = []
 	@State private var width: CGFloat = 100
+	@State private var imagesAreLoading = false
 
 	var body: some View {
-		let spacing: CGFloat = 12
-		let columns = Array(repeating: GridItem(.flexible(), spacing: spacing), count: 4)
-		let itemWidth = (width - spacing * 3) / 4
-		let rows = Int(ceil(Double(selectedImages.count) / 4))
-		let visibleRows = min(rows, 4)
-		let gridHeight = itemWidth * CGFloat(visibleRows)
-		+ spacing * CGFloat(max(visibleRows - 1, 0))
-
 		VStack(alignment: .leading, spacing: 8) {
 			HStack {
 				Text("Photos & Videos")
@@ -29,80 +22,96 @@ struct PhotosAndVideosView: View {
 			}
 
 			VStack(spacing: 16) {
-				if !selectedImages.isEmpty {
-						ScrollView {
-							LazyVGrid(columns: columns, spacing: spacing) {
-								ForEach(selectedImages.indices, id: \.self) { i in
-									selectedImages[i]
-										.resizable()
-										.scaledToFill()
-										.frame(width: itemWidth, height: itemWidth)
-										.clipped()
-										.cornerRadius(8)
-								}
-							}
-						}
-						.scrollDisabled(selectedImages.count <= 16)
-						.frame(height: gridHeight)
-				}
+				if imagesAreLoading {
 
-				PhotosPicker(
-					selection: $selectedItems,
-					photoLibrary: .shared()
-				) {
-					if selectedItems.isEmpty {
-						VStack(spacing: 12) {
-							Image(systemName: "arrow.up.circle")
-								.font(.system(size: 36))
-								.foregroundColor(Color.purple)
-							Text("Add photos or videos")
-								.font(.subheadline)
-								.foregroundColor(.primary)
-							Text("Tap to upload media")
-								.font(.footnote)
-								.foregroundColor(.gray)
-							HStack(spacing: 16) {
-								Image(systemName: "photo.on.rectangle")
-								Image(systemName: "video")
-							}
-							.foregroundColor(.gray)
-						}
-					} else {
-						HStack(alignment: .center, spacing: 16) {
-							Image(systemName: "plus")
-							Text("Add more").font(.headline)
-						}.foregroundStyle(.black)
-							.frame(height: 48)
-							.frame(maxWidth: .infinity)
-							.padding(.horizontal, 16)
-							.overlay(
-								RoundedRectangle(cornerRadius: 12)
-									.stroke(Color(.systemGray4), lineWidth: 1)
-							)
-					}
+				} else {
+					notLoadingViews()
 				}
-			}
-			.frame(maxWidth: .infinity)
-			.padding()
-			.background(
-				RoundedRectangle(cornerRadius: 12)
-					.strokeBorder(Color(.systemGray4), style: StrokeStyle(lineWidth: 1, dash: [4]))
-			)
-			.onChange(of: selectedItems) {
-				selectedImages.removeAll()
-				Task {
-					for item in selectedItems {
-						if let loadedImage = try? await item.loadTransferable(type: Image.self) {
-							selectedImages.append(loadedImage)
-						}
-					}
+			}.frame(maxWidth: .infinity)
+				.padding()
+				.background{
+					RoundedRectangle(cornerRadius: 12)
+						.strokeBorder(Color(.systemGray4), style: StrokeStyle(lineWidth: 1, dash: [4]))
 				}
-			}
 		}
 		.overlay {
 			GeometryReader { proxy in
 				Color.clear.task(id: proxy.size) {
 					width = proxy.size.width
+				}
+			}
+		}
+	}
+
+	@ViewBuilder
+	private func notLoadingViews() -> some View {
+		let spacing: CGFloat = 12
+		let columns = Array(repeating: GridItem(.flexible(), spacing: spacing), count: 4)
+		let itemWidth = (width - spacing * 3) / 4
+		let rows = Int(ceil(Double(selectedImages.count) / 4))
+		let visibleRows = min(rows, 4)
+		let gridHeight = itemWidth * CGFloat(visibleRows)
+		+ spacing * CGFloat(max(visibleRows - 1, 0))
+
+		if !selectedImages.isEmpty {
+			ScrollView {
+				LazyVGrid(columns: columns, spacing: spacing) {
+					ForEach(selectedImages.indices, id: \.self) { i in
+						selectedImages[i]
+							.resizable()
+							.scaledToFill()
+							.frame(width: itemWidth, height: itemWidth)
+							.clipped()
+							.cornerRadius(8)
+					}
+				}
+			}
+			.scrollDisabled(selectedImages.count <= 16)
+			.frame(height: gridHeight)
+		}
+
+		PhotosPicker(
+			selection: $selectedItems,
+			photoLibrary: .shared()
+		) {
+			if selectedItems.isEmpty {
+				VStack(spacing: 12) {
+					Image(systemName: "arrow.up.circle")
+						.font(.system(size: 36))
+						.foregroundColor(Color.purple)
+					Text("Add photos or videos")
+						.font(.subheadline)
+						.foregroundColor(.primary)
+					Text("Tap to upload media")
+						.font(.footnote)
+						.foregroundColor(.gray)
+					HStack(spacing: 16) {
+						Image(systemName: "photo.on.rectangle")
+						Image(systemName: "video")
+					}
+					.foregroundColor(.gray)
+				}
+			} else {
+				HStack(alignment: .center, spacing: 16) {
+					Image(systemName: "plus")
+					Text("Add more").font(.headline)
+				}.foregroundStyle(.black)
+					.frame(height: 48)
+					.frame(maxWidth: .infinity)
+					.padding(.horizontal, 16)
+					.overlay(
+						RoundedRectangle(cornerRadius: 12)
+							.stroke(Color(.systemGray4), lineWidth: 1)
+					)
+			}
+		}
+		.onChange(of: selectedItems) {
+			selectedImages.removeAll()
+			Task {
+				for item in selectedItems {
+					if let loadedImage = try? await item.loadTransferable(type: Image.self) {
+						selectedImages.append(loadedImage)
+					}
 				}
 			}
 		}
