918237ee38de2a5b77c325d22682c66707acf4e0
Samuel Martins <fsamuelsmartins@gmail.com>
2025-12-15 13:43:09 -0300

    refacotirng code

--- TimeCapsule/DailyLog/Components/InputContainerView.swift
+++ TimeCapsule/DailyLog/Components/InputContainerView.swift
@@ -14,122 +14,13 @@ struct InputContainerView: View {
 
 	var body: some View {
 		VStack(alignment: .leading, spacing: 12) {
-			HStack(alignment: .center) {
-				Button {
-					isSendOptionsExpanded.toggle()
-					isMoodSelected = false
-				} label : {
-					Image(systemName: "plus")
-						.resizable()
-						.frame(width: 16, height: 16)
-						.transaction { transaction in transaction.animation = nil }
-						.foregroundStyle(.black)
-				}
-
-				HStack {
-					TextEditor(text: $thoughtsText)
-						.focused(isFocused)
-						.scrollContentBackground(.hidden)
-						.background(.white)
-						.frame(minHeight: initialHeight, maxHeight: 120)
-						.fixedSize(horizontal: false, vertical: true)
-						.overlay(alignment: .leading) {
-							if thoughtsText.isEmpty {
-								Text("Write your thoughts")
-									.foregroundColor(Color(.placeholderText))
-									.padding(.horizontal, 4)
-							}
-						}
-
-					VStack {
-						if let emoji = currentMood?.emoji {
-							Text(emoji)
-						} else {
-							Circle()
-								.stroke(
-									Color.yellow,
-									style: StrokeStyle(
-										lineWidth: 2,
-										dash: [6, 4]
-									)
-								)
-								.frame(width: 20, height: 20)
-						}
-					}
-					.frame(width: 24, height: 24)
-					.contentShape(Rectangle())
-					.onTapGesture {
-						isMoodSelected.toggle()
-						isSendOptionsExpanded = false
-					}
-					.onLongPressGesture(minimumDuration: 0.5) {
-						isMoodSelected = false
-						isSendOptionsExpanded = false
-						currentMood = nil
-					}
-					.animation(.default, value: currentMood)
-					.transaction { transaction in transaction.animation = nil }
-				}
-				.padding(.leading, 4)
-				.padding(.trailing, 6)
-				.background(.white)
-				.clipShape(RoundedRectangle(cornerRadius: initialHeight))
-
-				HStack(spacing: 16) {
-					if thoughtsText.isEmpty {
-						Button {
-						} label : {
-							Image(systemName: "microphone")
-								.resizable()
-								.scaledToFit()
-								.frame(width: 20, height: 20)
-								.padding(.leading, 4)
-						}
-
-						Button {
-						} label : {
-							Image(systemName: "camera")
-								.resizable()
-								.scaledToFit()
-								.frame(width: 20, height: 20)
-						}
-					} else {
-						RoundButtonView(systemImageName: "paperplane", colors: [.green]) {}
-							.foregroundStyle(.white)
-							.frame(width: initialHeight)
-					}
-				}
-				.foregroundStyle(.black)
-				.animation(.default, value: thoughtsText)
-			}
-			.padding(.horizontal)
+			buildMainView().padding(.horizontal)
 
 			if isSendOptionsExpanded || isMoodSelected {
 				Divider()
 			}
 
-			VStack {
-				if isMoodSelected {
-					MoodSliderView(currentMood: $currentMood)
-				}
-
-				if isSendOptionsExpanded {
-					HStack(alignment: .top, spacing: 20) {
-						ForEach(InputOption.allCases) { option in
-							RoundButtonView(
-								systemImageName: option.systemImageName,
-								colors: option.gradientColors,
-								text: option.title
-							) {
-								handleOptionSelection(option)
-							}
-						}
-					}
-					.frame(maxHeight: 90)
-					.frame(maxWidth: .infinity)
-					.tint(.white)
-				}
-			}
+			buildHiddenOptions()
 			.padding()
 			.frame(height: isSendOptionsExpanded || isMoodSelected ? 140 : 0)
 
@@ -147,10 +38,126 @@ struct InputContainerView: View {
 				isMoodSelected = false
 			}
 		}
+	}
+
+	@ViewBuilder
+	private func buildMainView() -> some View {
+		HStack(alignment: .center) {
+			Button {
+				isSendOptionsExpanded.toggle()
+				isMoodSelected = false
+			} label : {
+				Image(systemName: "plus")
+					.resizable()
+					.frame(width: 16, height: 16)
+					.transaction { transaction in transaction.animation = nil }
+					.foregroundStyle(.black)
+			}
+
+			HStack {
+				TextEditor(text: $thoughtsText)
+					.focused(isFocused)
+					.scrollContentBackground(.hidden)
+					.background(.white)
+					.frame(minHeight: initialHeight, maxHeight: 120)
+					.fixedSize(horizontal: false, vertical: true)
+					.overlay(alignment: .leading) {
+						if thoughtsText.isEmpty {
+							Text("Write your thoughts")
+								.foregroundColor(Color(.placeholderText))
+								.padding(.horizontal, 4)
+						}
+					}
+
+				VStack {
+					if let emoji = currentMood?.emoji {
+						Text(emoji)
+					} else {
+						Circle()
+							.stroke(
+								Color.yellow,
+								style: StrokeStyle(
+									lineWidth: 2,
+									dash: [6, 4]
+								)
+							)
+							.frame(width: 20, height: 20)
+					}
+				}
+				.frame(width: 24, height: 24)
+				.contentShape(Rectangle())
+				.onTapGesture {
+					isMoodSelected.toggle()
+					isSendOptionsExpanded = false
+				}
+				.onLongPressGesture(minimumDuration: 0.5) {
+					isMoodSelected = false
+					isSendOptionsExpanded = false
+					currentMood = nil
+				}
+				.animation(.default, value: currentMood)
+				.transaction { transaction in transaction.animation = nil }
+			}
+			.padding(.leading, 4)
+			.padding(.trailing, 6)
+			.background(.white)
+			.clipShape(RoundedRectangle(cornerRadius: initialHeight))
+
+			HStack(spacing: 20) {
+				if thoughtsText.isEmpty {
+					Button {
+					} label : {
+						Image(systemName: "microphone")
+							.resizable()
+							.scaledToFit()
+							.frame(width: 20, height: 20)
+							.padding(.leading, 4)
+					}
+					Button {
+					} label : {
+						Image(systemName: "camera")
+							.resizable()
+							.scaledToFit()
+							.frame(width: 20, height: 20)
+					}
+				} else {
+					RoundButtonView(systemImageName: "paperplane", colors: [.green]) {}
+						.foregroundStyle(.white)
+						.frame(width: initialHeight)
+				}
+			}
+			.foregroundStyle(.black)
+			.animation(.default, value: thoughtsText)
+		}
 
 	}
 
-	// Helper to handle actions neatly
+	@ViewBuilder
+	private func buildHiddenOptions() -> some View {
+		VStack {
+			if isMoodSelected {
+				MoodSliderView(currentMood: $currentMood)
+			}
+
+			if isSendOptionsExpanded {
+				HStack(alignment: .top, spacing: 20) {
+					ForEach(InputOption.allCases) { option in
+						RoundButtonView(
+							systemImageName: option.systemImageName,
+							colors: option.gradientColors,
+							text: option.title
+						) {
+							handleOptionSelection(option)
+						}
+					}
+				}
+				.frame(maxHeight: 90)
+				.frame(maxWidth: .infinity)
+				.tint(.white)
+			}
+		}
+	}
+
 	private func handleOptionSelection(_ option: InputOption) {
 		switch option {
 		case .camera:
